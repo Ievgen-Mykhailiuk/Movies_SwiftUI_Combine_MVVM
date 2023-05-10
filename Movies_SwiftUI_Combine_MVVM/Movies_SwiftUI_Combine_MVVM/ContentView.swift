fa2be3068a09bc5ee7b62e23bb97304f5dec783c
IevgenMykhailiuk <ievgen.mykhailiuk@gmail.com>
2023-05-10 12:02:00 +0200

    create project

---
+++ Movies_SwiftUI_Combine_MVVM/Movies_SwiftUI_Combine_MVVM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movies_SwiftUI_Combine_MVVM
+//
+//  Created by Евгений  on 10/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movies_SwiftUI_Combine_MVVM/Movies_SwiftUI_Combine_MVVM/Movies_SwiftUI_Combine_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_SwiftUI_Combine_MVVMApp.swift
+//  Movies_SwiftUI_Combine_MVVM
+//
+//  Created by Евгений  on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Movies_SwiftUI_Combine_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
